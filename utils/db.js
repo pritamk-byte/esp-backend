cf3abc593c2c8e5716c40fca01808e365c5bd86f
impritamk <im.pritamk@gmail.com>
2026-05-27 19:05:55 +0530

    cc

--- utils/db.js
+++ utils/db.js
@@ -1,11 +1,6 @@
 const { PrismaClient } = require('@prisma/client');
 
-// This pattern stops Node from opening 50 connections by accident
-const globalForPrisma = global;
-const prisma = globalForPrisma.prisma || new PrismaClient();
-
-if (process.env.NODE_ENV !== 'production') {
-    globalForPrisma.prisma = prisma;
-}
+// Simple, direct instantiation for Node.js backend
+const prisma = new PrismaClient();
 
 module.exports = prisma;
